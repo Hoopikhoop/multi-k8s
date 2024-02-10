c5d79034874af309c67994096035fb5713a98d1a
Hoopikhoop <hoopihoop98@gmail.com>
2024-02-10 17:20:20 +0700

    update docker build versions

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ In order for postgres to work correctly, you need to add a secret:
 kubectl create secret generic pgpassword --from-literal PGPASSWORD=(your_password)
 
 You dont need to login again in deploy.sh file because you will already be logged in the travis.yml.
+$GIT_SHA is used to indicate docker build versions (Easier to debug, solves the problem of the "latest" tag)
--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,16 @@
-docker build -t hoopikhoop/multi-client -f ./client/Dockerfile ./client
-docker build -t hoopikhoop/multi-server -f ./server/Dockerfile ./server
-docker build -t hoopikhoop/multi-worker -f ./worker/Dockerfile ./worker
-docker push hoopikhoop/multi-client
-docker push hoopikhoop/multi-server
-docker push hoopikhoop/multi-worker
+docker build -t hoopikhoop/multi-client:latest -t hoopikhoop/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t hoopikhoop/multi-server:latest -t hoopikhoop/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t hoopikhoop/multi-worker:latest -t hoopikhoop/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push hoopikhoop/multi-client:latest
+docker push hoopikhoop/multi-server:latest
+docker push hoopikhoop/multi-worker:latest
+
+docker push hoopikhoop/multi-client:$SHA
+docker push hoopikhoop/multi-server:$SHA
+docker push hoopikhoop/multi-worker:$SHA
+
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=hoopikhoop/multi-server
+kubectl set image deployments/client-deployment client=hoopikhoop/multi-client:$SHA
+kubectl set image deployments/server-deployment server=hoopikhoop/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=hoopikhoop/multi-worker:$SHA
